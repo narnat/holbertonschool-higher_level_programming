6d21e4a69a2e5cd6391b7d4d86985f0e331da15e
Farrukh <679@holbertonschool.com>
2019-05-02 17:23:40 +0000

    Prints arguments of script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,5 @@ elif l == 1:
     print("{0} argument:\n{0}: {1}".format(l, sys.argv[l]))
 else:
     print("{} arguments:".format(l))
-    arg = sys.argv[1:]
-    for i, str in enumerate(arg, start=1):
+    for i, str in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, str))
