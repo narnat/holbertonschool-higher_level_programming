5fcb4a06ebd60c04079858e48b6b1b0c47eb7e9f
Farrukh <679@holbertonschool.com>
2019-05-02 18:07:09 +0000

    Refractored code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    res = 0
-    for i in sys.argv[1:]:
-        res += int(i)
-    print(res)
+    print(sum(int(i) for i in sys.argv[1:]))
