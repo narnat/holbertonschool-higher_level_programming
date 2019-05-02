372ed5f5b5032acb5eb8dc5d1776ade438aa1c51
Farrukh <679@holbertonschool.com>
2019-05-02 20:12:54 +0000

    Changed print statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     l = len(sys.argv)
     if l != 4:
-        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
