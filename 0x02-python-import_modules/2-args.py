9ea1dda96910ee85e8b4589a5ad06155b29643ad
Farrukh <679@holbertonschool.com>
2019-05-02 17:37:36 +0000

    Added __main__ condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import sys
-l = len(sys.argv) - 1
-if l == 0:
-    print("{} arguments.".format(l))
-elif l == 1:
-    print("{0} argument:\n{0}: {1}".format(l, sys.argv[l]))
-else:
-    print("{} arguments:".format(l))
-    for i, str in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, str))
+def func():
+    import sys
+    l = len(sys.argv) - 1
+    if l == 0:
+        print("{} arguments.".format(l))
+    elif l == 1:
+        print("{0} argument:\n{0}: {1}".format(l, sys.argv[l]))
+    else:
+        print("{} arguments:".format(l))
+        for i, str in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, str))
+if __name__ == "__main__":
+    func()
