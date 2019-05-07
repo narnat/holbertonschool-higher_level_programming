1d43abe2c71442014f267cbfdfa9146b182fdcef
Farrukh <679@holbertonschool.com>
2019-05-07 19:22:00 +0000

    Printing reverse string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if (my_list == []):
+        print()
+        return
     for i in my_list[::-1]:
         print("{:d}".format(i))
