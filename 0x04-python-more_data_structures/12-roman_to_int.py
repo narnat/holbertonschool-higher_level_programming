06a9c4f47a07e556749fd98fc896068ce066238e
Farrukh <679@holbertonschool.com>
2019-05-09 22:36:08 +0000

    Changed if condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
-        return (0)
-    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    n = 0
-    prev = 0
-    for c in roman_string:
-        if (c in d):
-            if (d[c] > prev):
-                n = d[c] - n
-            else:
-                n += d[c]
-            prev = d[c]
-    return n
+    if isinstance(roman_string, str):
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        n = 0
+        prev = 0
+        for c in roman_string:
+            if (c in d):
+                if (d[c] > prev):
+                    n = d[c] - n
+                else:
+                    n += d[c]
+                    prev = d[c]
+                    return n
+    return 0
