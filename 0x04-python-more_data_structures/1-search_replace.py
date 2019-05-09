33d2e3f774b2904ed68a128d4a180544ca399968
Farrukh <679@holbertonschool.com>
2019-05-09 17:13:05 +0000

    Refractored code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n = my_list.copy()
-    for i, el in enumerate(n):
-        if (el == search):
-            n[i] = replace
-    return n
+    return [replace if i == search else i for i in my_list]
