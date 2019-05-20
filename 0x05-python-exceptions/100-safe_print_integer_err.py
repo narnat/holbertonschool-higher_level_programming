23a34b4fa9bdf9a5db16e10671a9500561a06d3b
Farrukh <679@holbertonschool.com>
2019-05-20 19:24:31 +0000

    Removed space

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print("Exception:", e, file=sys.stderr)
         return False
