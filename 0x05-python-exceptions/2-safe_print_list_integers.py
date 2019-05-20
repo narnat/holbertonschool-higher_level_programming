d6b2179787111a404a31b38451ddfa057a844192
Farrukh <679@holbertonschool.com>
2019-05-20 19:37:08 +0000

    Refractored code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
-    if x > 0:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                c += 1
-            except:
-                continue
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+        except:
+            continue
+    print()
     return c
