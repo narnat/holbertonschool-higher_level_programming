86b1b6bce9db095a522680bce3f1f18000551807
Farrukh <679@holbertonschool.com>
2019-05-20 19:29:27 +0000

    Refractored code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    if x > 0:
-        try:
-            for i in range(x):
-                print(my_list[i], end="")
-            print()
-            return i + 1
-        except:
-            print()
-            return i
-    else:
-        return i
+    c = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            c += 1
+    except:
+        pass
+    print()
+    return c
