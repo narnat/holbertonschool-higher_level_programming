d106db771cdd6f505a3bb2c5b818a7545badf6d9
Farrukh <679@holbertonschool.com>
2019-05-21 23:15:55 +0000

    Magic Circle task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Magic Class"""
+
+
+import math
+class MagicClass:
+    """Magic Class"""
+    def __init__(self, radius=0):
+        """Constructor"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Area of a circle"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Circumference of a circle"""
+        return 2 * math.pi * self.__radius
+s = MagicClass(3.4)
+print(s.circumference())
