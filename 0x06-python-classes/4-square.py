dd82285174f0666e16f4e48d36dac5778a2ecf2d
Farrukh <679@holbertonschool.com>
2019-05-21 23:13:28 +0000

    Added 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Simple class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Constructor"""
+        self.size = size
+
+    def area(self):
+        """Area of a square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter function for __size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function for __size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
