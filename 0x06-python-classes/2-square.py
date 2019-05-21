a5b7c25f2d6c7c1aa1fc16cf94d2c5445f02fb2b
Farrukh <679@holbertonschool.com>
2019-05-21 17:47:47 +0000

    Initialize size with type checking

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Simple class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Constructor"""
+        try:
+            size += 0
+        except:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
