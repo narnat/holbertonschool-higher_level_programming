59a07988a4f2227fef8c96f949bb3294e0962497
Farrukh <679@holbertonschool.com>
2019-05-21 23:12:15 +0000

    Added square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,7 @@ class Square:
     """Square class"""
     def __init__(self, size=0):
         """Constructor"""
-        try:
-            size += 0
-        except:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
