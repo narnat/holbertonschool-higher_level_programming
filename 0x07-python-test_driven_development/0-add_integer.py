eb6328ce6eb15bfb1543cca6ce14bb84df5bafb2
Farrukh <679@holbertonschool.com>
2019-05-27 19:20:37 +0000

    Add test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,7 +15,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError('b must be an integer')
-    res = a + b
-    if res == float('inf') or res == -float('inf'):
-        raise TypeError("a must be an integer")
     return int(a) + int(b)
