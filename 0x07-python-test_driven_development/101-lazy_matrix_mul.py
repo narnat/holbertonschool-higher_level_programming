a7a4c0e717aa3f83dacb68365f749d02c9cfe096
Farrukh <679@holbertonschool.com>
2019-05-28 00:00:16 +0000

    Add error messages

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,58 +7,43 @@ import numpy
 def lazy_matrix_mul(m_a, m_b):
     """Multiplies matrix"""
     if type(m_a) != list:
-        raise ValueError("matmul: Input operand 0 does not have enough\
- dimensions (has 0, gufunc core with\
- signature (n?,k),(k,m?)->(n?,m?) requires 1)")
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
 
     if type(m_b) != list:
-        raise ValueError("matmul: Input operand 1 does not have enough\
- dimensions (has 0, gufunc core with\
- signature (n?,k),(k,m?)->(n?,m?) requires 1)")
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
 
     if any(type(i) != list for i in m_a):
-        raise ValueError("matmul: Input operand 0 does not have enough\
- dimensions (has 0, gufunc core with signature\
- (n?,k),(k,m?)->(n?,m?) requires 1)")
+        raise ValueError("shapes ({},{}) and ({},{}) not aligned: 0 (dim 1)\
+ != 2 (dim 0)".format(len(m_a), len(m_a[0])), len(m_b), len(m_b[0]))
 
     if any(type(i) != list for i in m_b):
-        raise ValueError("matmul: Input operand 1 does not have enough\
- dimensions (has 0, gufunc core with signature\
- (n?,k),(k,m?)->(n?,m?) requires 1)")
+        raise ValueError("shapes ({},{}) and ({},{}) not aligned: 2 (dim 1)\
+ != 0 (dim 0)".format(len(m_a), len(m_a[0])), len(m_b), len(m_b[0]))
+
 
     if any(not i for i in m_a):
-        raise ValueError("matmul: Input operand 0 does not have enough\
- dimensions (has 0, gufunc core with\
- signature (n?,k),(k,m?)->(n?,m?) requires 1)")
+        raise ValueError("shapes ({},{}) and ({},{}) not aligned: 0 (dim 1)\
+ != 2 (dim 0)".format(len(m_a), len(m_a[0])), len(m_b), len(m_b[0]))
 
     if any(not i for i in m_b):
-        raise ValueError("matmul: Input operand 1 does not have enough\
- dimensions (has 0, gufunc core with\
- signature (n?,k),(k,m?)->(n?,m?) requires 1)")
+        raise ValueError("shapes ({},{}) and ({},{}) not aligned: 2 (dim 1)\
+ != 0 (dim 0)".format(len(m_a), len(m_a[0])), len(m_b), len(m_b[0]))
 
     if any(any(type(j) != int and type(j) != float for j in i) for i in m_a):
-        raise TypeError("ufunc 'matmul' did not contain a loop with signature \
-matching types dtype('<U21') dtype('<U21') dtype('<U21')")
+        raise TypeError("invalid data type for einsum")
 
     if any(any(type(j) != int and type(j) != float for j in i) for i in m_b):
-        raise TypeError("ufunc 'matmul' did not contain a loop with signature \
-matching types dtype('<U21') dtype('<U21') dtype('<U21')")
+        raise TypeError("invalid data type for einsum")
 
     l_a = len(m_a[0])
     if any(len(row) != l_a for row in m_a):
-        raise TypeError("ufunc 'matmul' not supported for the input types,\
- and the inputs could not be safely coerced to any\
- supported types according to the casting rule ''safe''")
+        raise TypeError("setting an array element with a sequence.")
 
     l_b = len(m_b[0])
     if any(len(row) != l_b for row in m_b):
-        raise TypeError("ufunc 'matmul' not supported for the input types,\
- and the inputs could not be safely coerced to any\
- supported types according to the casting rule ''safe''")
+        raise TypeError("setting an array element with a sequence.")
 
     if len(m_a[0]) != len(m_b):
-        raise ValueError("matmul: Input operand 1 has a mismatch in its core \
-dimension 0, with gufunc signature (n?,k),(k,m?)->(n?,m?) \
-(size {} is different from {})".format(len(m_b), len(m_a[0])))
-
+        raise ValueError("shapes ({},{}) and ({},{}) not aligned: 0 (dim 1)\
+ != 2 (dim 0)".format(len(m_a), len(m_a[0])), len(m_b), len(m_b[0]))
     return numpy.matmul(m_a, m_b)
