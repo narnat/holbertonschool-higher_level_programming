5ad8b95e4d5f12b01992811e946f5f4321e40d94
Farrukh <679@holbertonschool.com>
2019-05-28 00:48:52 +0000

    Fix err messages

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -22,11 +22,11 @@ def lazy_matrix_mul(m_a, m_b):
 
     if any(not i for i in m_a):
         raise ValueError("shapes ({},{}) and ({},{}) not aligned: 0 (dim 1)\
- != 2 (dim 0)".format(len(m_a), len(m_a[0])), len(m_b), len(m_b[0]))
+ != 2 (dim 0)".format(len(m_a), len(m_a[0]), len(m_b), len(m_b[0])))
 
     if any(not i for i in m_b):
         raise ValueError("shapes ({},{}) and ({},{}) not aligned: 2 (dim 1)\
- != 0 (dim 0)".format(len(m_a), len(m_a[0]), len(m_b), len(m_b[0])))
+ != 1 (dim 0)".format(len(m_a), len(m_a[0]), len(m_b), len(m_b[0])))
 
     if any(any(type(j) != int and type(j) != float for j in i) for i in m_a):
         raise TypeError("invalid data type for einsum")
@@ -43,7 +43,6 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("setting an array element with a sequence.")
 
     if len(m_a[0]) != len(m_b):
-        raise ValueError("shapes ({},{}) and ({},{}) not aligned: 0 (dim 1)\
- != 2 (dim 0)".format(len(m_a), len(m_a[0]), len(m_b), len(m_b[0])))
+        raise ValueError("shapes ({0},{1}) and ({2},{3}) not aligned: {1}\
+ (dim 1) != {2} (dim 0)".format(len(m_a), len(m_a[0]), len(m_b), len(m_b[0])))
     return numpy.matmul(m_a, m_b)
-
