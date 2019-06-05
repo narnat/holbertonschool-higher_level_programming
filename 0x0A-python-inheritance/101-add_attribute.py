7dabf8426e92e89a9f013b9c6b99334e0d6598a4
Farrukh <679@holbertonschool.com>
2019-06-05 20:37:55 +0000

    Adds an attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,9 @@
 
 def add_attribute(cls, name, value):
     """Adds attribute to cls"""
-    if cls.__class__.__name__.islower():
+    if "__dict__" not in dir(cls) and "__slots__" not in dir(cls):
         raise TypeError("can't add new attribute")
+    if hasattr(cls, "__slots__"):
+        if name not in cls.__slots__:
+            raise TypeError("can't add new attribute")
     cls.name = value
