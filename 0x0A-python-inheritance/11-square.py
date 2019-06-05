002976bd702d5a314588caf79f1e4397042ceeb7
Farrukh <679@holbertonschool.com>
2019-06-05 18:11:05 +0000

    Add __str__ method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class, inherits from Rectangle class"""
+
+    def __init__(self, size):
+        """Init method of Square"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """String method for Square"""
+        return "[Square] {0}/{0}".format(self.__size)
