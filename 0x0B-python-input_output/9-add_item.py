1c1a8298434828f6c5b285f509acc90608c7ee48
Farrukh <679@holbertonschool.com>
2019-06-06 17:18:54 +0000

    Refractoring

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,13 +7,7 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
-le = len(argv)
-storage = [None] * (le - 1)
-
-for i, el in enumerate(argv[1:]):
-    storage[i] = el
-
 res = []
 if os.path.exists(filename):
     res = load_from_json_file(filename)
-save_to_json_file(res + storage, filename)
+save_to_json_file(res + argv[1:], filename)
