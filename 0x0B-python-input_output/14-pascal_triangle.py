4df3c9eecaa044089675059b4a7709460f03bd78
Farrukh <679@holbertonschool.com>
2019-06-06 23:01:16 +0000

    Pascal Triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+from math import factorial as fc
+
+
+def pascal_triangle(n):
+    """Pascal Triangle"""
+    a = []
+    for i in range(n):
+        b = []
+        for j in range(i + 1):
+            b.append(int(fc(i)/(fc(j) * fc(i - j))))
+        a.append(b)
+    return a
