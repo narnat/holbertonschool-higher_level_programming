3f5bdf98ad59a7c5ef5df9b72dd7280e4a79bbf2
Farrukh <679@holbertonschool.com>
2019-06-06 15:30:18 +0000

    Changed to hasattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(cls, name, value):
     """Adds attribute to cls"""
-    if "__dict__" not in dir(cls) and "__slots__" not in dir(cls):
+    if not hasattr(cls, '__dict__') and not hasattr(cls, '__slots__'):
         raise TypeError("can't add new attribute")
     if hasattr(cls, "__slots__"):
         if name not in cls.__slots__:
