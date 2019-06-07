9fd3baf8be817809f1cd0df57a935b67625d1ec4
Farrukh <679@holbertonschool.com>
2019-06-07 05:59:48 +0000

    Fix SIGINT

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Log Parser"""
-import signal
-from sys import stdin
 
 
 def printer(size, d):
@@ -17,16 +15,16 @@ d = {"200": 0, "301": 0, "400": 0, "401": 0,
      "403": 0, "404": 0, "405": 0, "500": 0}
 
 
-def handler(signum, frame):
-    printer(size, d)
-
 counter = 1
-signal.signal(signal.SIGINT, handler)
-# with open(0) as f:
-for line in stdin:
-    arr = line.split()
-    size += int(arr[-1])
-    d[arr[-2]] += 1
-    if counter % 10 == 0:
-        printer(size, d)
-    counter += 1
+try:
+    with open(0) as f:
+        for line in f:
+            arr = line.split()
+            size += int(arr[-1])
+            d[arr[-2]] += 1
+            if counter % 10 == 0:
+                printer(size, d)
+            counter += 1
+except KeyboardInterrupt:
+    printer(size, d)
+    raise
