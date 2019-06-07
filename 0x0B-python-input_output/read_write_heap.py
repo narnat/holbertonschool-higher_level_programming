b43eeaa57fa1beab64c3115d67b7d08158f9d24d
Farrukh <679@holbertonschool.com>
2019-06-07 20:56:09 +0000

    Condition for empty string

--- 0x0B-python-input_output/read_write_heap.py
+++ 0x0B-python-input_output/read_write_heap.py
@@ -33,8 +33,13 @@ for line in maps_file:
     if sline[-1][:-1] != "[heap]":
         continue
 
-    addr, perm, offset, device, inode = sline[:5]
-    pathname = sline[-1][:-1]
+    try:
+        addr, perm, offset, device, inode = sline[:5]
+        pathname = sline[-1][:-1]
+    except:
+        print("Error")
+        maps_file.close()
+        sys.exit(1)
 
     if perm[0] != 'r' or perm[1] != 'w':
         print("{} does not have read/write permission".format(pathname))
@@ -56,8 +61,14 @@ for line in maps_file:
         maps_file.close()
         sys.exit(1)
 
-    mem_file.seek(addr_start)
-    heap = mem_file.read(addr_end - addr_start)
+    try:
+        mem_file.seek(addr_start)
+        heap = mem_file.read(addr_end - addr_start)
+    except:
+        print("ERROR")
+        maps_file.close()
+        mem_file.close()
+        sys.exit(1)
 
     try:
         i = heap.index(bytes(search_string, "ASCII"))
@@ -67,10 +78,23 @@ for line in maps_file:
         mem_file.close()
         sys.exit(1)
 
-    print("***Done***")
-    mem_file.seek(addr_start + i)
-    mem_file.write(bytes(write_string, "ASCII"))
+    try:
+        mem_file.seek(addr_start + i)
+        if not write_string:
+            mem_file.write(bytes(0, "ASCII"))
+        else:
+            mem_file.write(bytes(write_string, "ASCII"))
+        print("***Done***")
+    except:
+        print("Error")
+        maps_file.close()
+        sys.exit(1)
 
-    maps_file.close()
-    mem_file.close()
+    try:
+        maps_file.close()
+        mem_file.close()
+    except:
+        print("Error")
+        maps_file.close()
+        sys.exit(1)
     break
