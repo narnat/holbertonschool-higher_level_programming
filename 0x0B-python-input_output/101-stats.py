bbf2a9b7bc8c9f25c737ba739579a4c0c3ed3af0
Farrukh <679@holbertonschool.com>
2019-06-07 06:07:37 +0000

    Fix SIGINT

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 """Log Parser"""
 
+if __name__ == '__main__':
 
-def printer(size, d):
-    """Printer function"""
-    a = sorted(d.keys())
-    print("File size:", size)
-    for i in a:
-        if d[i] != 0:
-            print("{}: {}".format(i, d[i]))
+    def printer(size, d):
+        """Printer function"""
+        a = sorted(d.keys())
+        print("File size: {:d}".format(size))
+        for i in a:
+            if d[i] != 0:
+                print("{}: {}".format(i, d[i]))
 
-size = 0
-d = {"200": 0, "301": 0, "400": 0, "401": 0,
-     "403": 0, "404": 0, "405": 0, "500": 0}
+    size = 0
+    d = {"200": 0, "301": 0, "400": 0, "401": 0,
+         "403": 0, "404": 0, "405": 0, "500": 0}
 
 
-counter = 1
-try:
-    with open(0) as f:
-        for line in f:
-            arr = line.split()
-            size += int(arr[-1])
-            d[arr[-2]] += 1
-            if counter % 10 == 0:
-                printer(size, d)
-            counter += 1
-except KeyboardInterrupt:
-    printer(size, d)
-    raise
+    counter = 1
+    try:
+        with open(0) as f:
+            for line in f:
+                arr = line.split()
+                size += int(arr[-1])
+                d[arr[-2]] += 1
+                if counter % 10 == 0:
+                    printer(size, d)
+                counter += 1
+    except KeyboardInterrupt:
+        printer(size, d)
+        raise
