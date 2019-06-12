fc2c359d8a7ebae37c0ac2b3f06d9a6fc17d1e9b
Farrukh <679@holbertonschool.com>
2019-06-12 16:53:08 +0000

    Change condition, removed conditon from create()

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,9 +30,9 @@ class Base:
         """To json the list of dictionaries"""
         if list_dictionaries in (None, []):
             return "[]"
-        if not isinstance(list_dictionaries, list) or\
-           not all(isinstance(d, dict) for d in list_dictionaries):
-            return "[]"
+        # if not isinstance(list_dictionaries, list) or\
+        #    not all(isinstance(d, dict) for d in list_dictionaries):
+        #     return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
