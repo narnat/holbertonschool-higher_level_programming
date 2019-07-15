b8ed802aea12826ad29f40837d153b1a46ce089d
Farrukh <679@holbertonschool.com>
2019-07-15 19:15:57 +0000

    Capitalize comamnds

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,3 @@
 -- Import in hbtn_0c_0 database this table dump
--- Loads table
-SOURCE temperatures.sql;
 -- Displays avg temp in July and August
-SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
