3f43c1f09e07facd2961f7ce448da2a16aa53b9a
Farrukh <679@holbertonschool.com>
2019-07-16 16:15:32 +0000

    Creates database and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Give privileges
-GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_2'@'localhost'
+-- Give privileges to user_0d_2
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
