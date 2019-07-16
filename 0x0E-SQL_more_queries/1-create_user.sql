e36b18f4ffa6eae83ad45ff8f5affccc38eba0b8
Farrukh <679@holbertonschool.com>
2019-07-16 16:04:16 +0000

    Creates a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates a user
+-- Creates user with all permissions
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
