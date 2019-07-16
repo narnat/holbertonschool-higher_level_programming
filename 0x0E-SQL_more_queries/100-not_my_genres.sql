68a6191c6534d062f7a5ad65897cb42d7994e16b
Farrukh <679@holbertonschool.com>
2019-07-16 20:24:31 +0000

    Add genres not in Dexter series

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,5 @@
 -- A script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
-SELECT DISTINCT tv_shows.title
+SELECT DISTINCT tv_genres.name
 FROM tv_show_genres
 LEFT JOIN tv_shows
 ON tv_show_genres.show_id=tv_shows.id
@@ -13,6 +13,6 @@ LEFT JOIN tv_shows
 ON tv_show_genres.show_id=tv_shows.id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_genres.name='Comedy'
+WHERE tv_shows.title='Dexter'
 )
-ORDER BY tv_shows.title;
+ORDER BY tv_genres.name;
