a8892064328ef91190d3e47caee88cc1fa793136
Farrukh <679@holbertonschool.com>
2019-07-16 17:00:32 +0000

    Add Foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+-- (in the database hbtn_0d_usa) on your MySQL server.
+-- Create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use hbtn_0d_usa
+USE hbtn_0d_usa;
+-- usage of primary key
+CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id),
+PRIMARY KEY (id));
+
