99fea609b5ac509a2be40d8090ff7b99d8ab5898
Farrukh <679@holbertonschool.com>
2019-08-20 21:37:07 +0000

    Changed the order

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catch me
-curl -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl "0.0.0.0:5000/catch_me" -LX PUT -H "Origin: HolbertonSchool" -d "user_id=98"
