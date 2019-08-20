84dfecade2b9859f37cdf16a765fe351cf976154
Farrukh <679@holbertonschool.com>
2019-08-20 20:05:21 +0000

    Add peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """Write a function that finds a peak in a list of unsorted integers."""
 
 
-def find_peak_2(list_of_integers):
+def find_peak(list_of_integers):
     """Function to find a peak in a list"""
     if list_of_integers == []:
         return None
@@ -10,17 +10,17 @@ def find_peak_2(list_of_integers):
     m = l // 2
     arr = list_of_integers
     while True:
-        if m != 0 and arr[m - 1] < arr[m] < arr[m + 1]:
+        if m != 0 and m != l - 1 and arr[m - 1] < arr[m] < arr[m + 1]:
             m += 1
-        elif m != 0 and arr[m - 1] > arr[m] > arr[m + 1]:
+        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] > arr[m + 1]:
             m -= 1
-        elif m != 0 and arr[m - 1] > arr[m] < arr[m + 1]:
+        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] < arr[m + 1]:
             m -= 1
         else:
             return arr[m]
 
 
-def find_peak(list_of_integers):
+def find_peak_2(list_of_integers):
     """Function to find a peak in a list"""
     if list_of_integers == []:
         return None
