47dc7a4055437826eb2dbd10bb5c5910782a7470
Farrukh <679@holbertonschool.com>
2019-08-26 22:29:26 +0000

    Add new printing

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
         print(i.get('name'))
         for a in i.get('films'):
             film = requests.get(a)
-            print('\t', film.json().get('title'), sep="")
+            print('{}: {}')
+            #print('\t', film.json().get('title'), sep="")
