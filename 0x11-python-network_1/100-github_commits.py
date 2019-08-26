44a4397854eb624939c8a633362bfc5d6b9e141b
Farrukh <679@holbertonschool.com>
2019-08-26 22:12:51 +0000

    Add GitHub API

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
     l = r.json()
-    for i in range(10):
-        print(l[i].get('sha'), l[i].get('commit')
-              .get('author').get('name'), sep=": ")
+    try:
+        for i in range(10):
+            print(l[i].get('sha'), l[i].get('commit')
+                  .get('author').get('name'), sep=": ")
+    except:
+        pass
