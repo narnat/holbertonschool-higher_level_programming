465f43ac9e92ebd131921a8aa28ed219a2b96a15
Farrukh <679@holbertonschool.com>
2019-08-26 20:29:50 +0000

    Add argv

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL and displays
 the value of the variable X-Request-Id in the response header"""
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(argv[1])
     print(r.headers.get('X-Request-Id'))
