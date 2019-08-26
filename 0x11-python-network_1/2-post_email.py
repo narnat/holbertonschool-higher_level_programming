15384a9934b050e10e2699ff1d8e03c41d8825ab
Farrukh <679@holbertonschool.com>
2019-08-26 19:57:48 +0000

    Post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ import urllib.request
 import urllib.parse
 from sys import argv
 
-
-post_data = urllib.parse.urlencode({'email': argv[2]}).encode('ascii')
-with urllib.request.urlopen(url=argv[1], data=post_data) as response:
-    print(response.read().decode('UTF-8'))
+if __name__ == "__main__":
+    post_data = urllib.parse.urlencode({'email': argv[2]}).encode('ascii')
+    with urllib.request.urlopen(url=argv[1], data=post_data) as response:
+        print(response.read().decode('UTF-8'))
