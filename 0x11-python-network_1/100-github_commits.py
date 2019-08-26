7dc6268e61d18dda586f022614991389ff7d754c
Farrukh <679@holbertonschool.com>
2019-08-26 21:19:46 +0000

    Revert back

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[1], argv[2]))
+                     .format(argv[2], argv[1]))
     l = r.json()
     for i in range(10):
         print(l[i].get('sha'), l[i].get('commit')
