27ccd03234544dea4b536b0c7c1f663e25391517
Farrukh <679@holbertonschool.com>
2019-08-26 08:09:27 +0000

    Add GET request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""urllib usage"""
+import urllib.request
+import urllib.parse
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type:', type(html))
+    print('\t- content:', html)
+    print('\t- utf8 content:', html.decode('UTF-8'))
