75038a53a0bf3b8f2adda55c1c9a12660f45bb7d
Farrukh <679@holbertonschool.com>
2019-08-27 20:21:17 +0000

    Add count 6

--- 0x11-python-network_1/103-search_twitter.py
+++ 0x11-python-network_1/103-search_twitter.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     search_headers = {'Authorization': 'Bearer {}'.format(access_token)}
 
-    search_params = {'q': argv[3], 'count': 5}
+    search_params = {'q': argv[3], 'count': 6}
 #    search_params = {'q': argv[3], 'result_type': 'recent', 'count': 5}
     search_url = '{}1.1/search/tweets.json'.format(base_url)
     search_resp = requests.get(search_url,
