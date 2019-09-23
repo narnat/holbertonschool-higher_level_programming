f3f8c081312574279b3e81888c48dfeaf1079f20
Farrukh <679@holbertonschool.com>
2019-09-23 23:04:54 +0000

    Add different code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,13 +2,22 @@
 
 const dict = require('./101-data').dict;
 
-const entries = Object.entries(dict);
+// const entries = Object.entries(dict);
+// const newDict = {};
+// for (const [key, value] of entries) {
+//   if (value in newDict) {
+//     newDict[value].push(key);
+//   } else {
+//     newDict[value] = [key];
+//   }
+// }
+
+const values = [...new Set(Object.keys(dict).map(function (key) { return dict[key]; }))];
+const keys = Object.keys(dict);
 const newDict = {};
-for (const [key, value] of entries) {
-  if (value in newDict) {
-    newDict[value].push(key);
-  } else {
-    newDict[value] = [key];
-  }
+
+for (let i = 0; i < values.length; i++) {
+  newDict[values[i]] = keys.map(function (x) { if (dict[x] === values[i]) { return (x); } }).filter(Boolean);
 }
+
 console.log(newDict);
