50e952fe972fb4e13e5fa655ffef557dad7ebfa7
Farrukh <679@holbertonschool.com>
2019-09-24 19:08:43 +0000

    Add if

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-request('http://swapi.co/api/people/18', function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).films.length);
-  }
-});
+if (process.argv[2]) {
+  request('http://swapi.co/api/people/18', function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).films.length);
+    }
+  });
+}
