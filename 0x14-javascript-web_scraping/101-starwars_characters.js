a392522510baf0cb194f27789259e8ab714b567a
Farrukh <679@holbertonschool.com>
2019-09-24 21:04:05 +0000

    Add sync code

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
 
+function send (person, idx) {
+  if (idx >= person.length) {
+    return;
+  }
+  request(person[idx], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      return send(person, ++idx);
+    }
+  });
+}
+
 const request = require('request');
 request(`http://swapi.co/api/films/${process.argv[2]}`, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const people = JSON.parse(body).characters;
-    (async function () {
-      for await (const person of people) {
-        await request(person, function (error, response, body) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(JSON.parse(body).name);
-          }
-        });
-      }
-    })();
+    send(people, 0);
   }
 });
