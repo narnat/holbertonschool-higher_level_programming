c1bb1a9b67dceee56d62f594032a571cd3be1de2
Farrukh <679@holbertonschool.com>
2019-09-24 19:27:08 +0000

    refractor

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,14 +5,16 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const res = JSON.parse(body).results;
-    const char = 'https://swapi.co/api/people/18/';
     let count = 0;
-    res.forEach(e => {
-      if (e.characters.includes(char)) {
-        count++;
-      }
-    });
+    if (process.argv[2] === 'http://swapi.co/api/films') {
+      const res = JSON.parse(body).results;
+      const char = 'https://swapi.co/api/people/18/';
+      res.forEach(e => {
+        if (e.characters.includes(char)) {
+          count++;
+        }
+      });
+    }
     console.log(count);
   }
 });
