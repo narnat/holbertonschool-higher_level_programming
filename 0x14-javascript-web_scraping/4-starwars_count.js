4c53e88305f0e9bc3381b53bd55016fd8922487a
Farrukh <679@holbertonschool.com>
2019-09-24 21:19:26 +0000

    Add 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,7 +8,7 @@ request(process.argv[2], function (error, response, body) {
     let count = 0;
     if (process.argv[2] === 'http://swapi.co/api/films') {
       const res = JSON.parse(body).results;
-      const char = 'https://swapi.co/api/people/18/';
+      const char = '/18/';
       res.forEach(e => {
         if (e.characters.includes(char)) {
           count++;
